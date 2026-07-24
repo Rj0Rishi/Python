fffd1c2b1edd4d3f220a4ee56f0697927fa754d3
Rishi <jaiswarrishi26@gmail.com>
2026-07-24 16:17:43 +0530

    Copy with the help of list in python

---
+++ Copy 1st list elements into 2nd list.py
@@ -0,0 +1,6 @@
+original_list = [10,20,30,40,50]
+copied_list = []
+for item in original_list:
+    copied_list.append(item)
+print("Original list : ",original_list)    
+print("Copied list : ",copied_list)
