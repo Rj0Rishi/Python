0389b6d6796b91b060e8874c743faad61b1cf007
Rishi <jaiswarrishi26@gmail.com>
2026-08-19 15:34:50 +0530

    Create Program 26 too.py

---
+++ Program 26 too.py
@@ -0,0 +1,10 @@
+people =[
+    {"name":"Alin","age":"25","city":"New York"},
+    {"name":"Bob","age":"30","city":"Los Angeles"},
+    {"name":"Charlie","age":"28","city":"Chicago"}
+]
+for person in people:
+    print("Name:",person["name"])
+    print("Age:",person["age"])
+    print("City:",person["city"])
+    print()
